dac76d7227355805aa82a8e79de10dfa35733f29
ayraso <ayserabiasoylu@gmail.com>
2023-03-15 21:47:40 +0300

    Add files via upload

---
+++ PasswordValidationProgram/Program.cs
@@ -0,0 +1,39 @@
+﻿OpenUserInterface();
+string password = getUserPassword();
+
+string getUserPassword()
+{
+    Console.SetCursorPosition((Console.WindowWidth - 32) / 2 + 2, Console.CursorTop - 2 );
+    string password = string.Empty;
+    while (Console.Read() != -1) // enter press edildiği clean değil. bunu düzelt
+    {
+        if(Console.Read() != (char)13)
+        {
+            password += Console.Read();
+            HideUserInput();
+        }
+
+    }
+    return password;
+}
+
+void HideUserInput()
+{
+    
+}
+
+void DisplayOnTheScreen(string s)
+{
+    Console.SetCursorPosition((Console.WindowWidth - s.Length) / 2, Console.CursorTop);
+    Console.WriteLine(s);
+}
+
+void OpenUserInterface()
+{
+    DisplayOnTheScreen($"______________________________");
+    DisplayOnTheScreen($"| Enter your password below:   |");
+    DisplayOnTheScreen($"------------------------------");
+    DisplayOnTheScreen($"|                              |");
+    DisplayOnTheScreen($"------------------------------");
+}
+
