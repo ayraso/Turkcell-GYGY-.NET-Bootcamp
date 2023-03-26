c2dc9c4a49458c3d8e4b6d2bb252641af65e2eba
ayser <ayser@CENTAURI>
2023-03-27 00:52:21 +0300

    addflight() method is implemented with dictionary

--- Solid Principles/SingleResponsibility/FlightService.cs
+++ Solid Principles/SingleResponsibility/FlightService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,15 @@ namespace SingleResponsibility
     public class FlightService
     {
         public FlightService() { }
-
+        public Dictionary<Guid, Flight> Flights = new Dictionary<Guid, Flight>();
         public void AddFlight(String companyName,
                               String pointOfDeparture, DateTime departureTime, String departureGate,
                               String pointOfArrival,   DateTime arrivalTime)
-        { 
-            
+        {
+            Flight newFlight = new Flight(companyName, pointOfDeparture, departureTime, departureGate, pointOfArrival, arrivalTime) { };
+            this.Flights.Add(newFlight.Id, newFlight);
         }
-          
         public void DeleteFlight(Flight flight) { }
         public void UpdateFlight(Flight flight) { }
-
-       
     }
 }
