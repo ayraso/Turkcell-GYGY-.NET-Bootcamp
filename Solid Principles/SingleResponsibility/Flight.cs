40ad0eb2a9d3d3be39af189a3c55003c31691404
ayser <ayser@CENTAURI>
2023-03-27 00:30:47 +0300

    initial draft of the example of single responsibility principe

---
+++ Solid Principles/SingleResponsibility/Flight.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    public class Flight
+    {
+        public Flight(String companyName,
+                      String pointOfDeparture, DateTime departureTime, String departureGate,
+                      String pointOfArrival, DateTime arrivalTime) 
+        {
+            this.Id = Guid.NewGuid();
+            this.CompanyName = companyName;
+            this.PointOfDeparture = pointOfDeparture;
+            this.DepartureTime = departureTime;
+            this.DepartureGate = departureGate;
+            this.PointOfArrival = pointOfArrival;
+            this.ArrivalTime = arrivalTime; 
+        }
+        public Guid Id { get; private set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public String PointOfArrival { get; set; }
+        public String PointOfDeparture { get; set; }
+        public String CompanyName { get; set; }
+        public String DepartureGate { get; set; }
+
+    }
+}
---
+++ Solid Principles/SingleResponsibility/FlightService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibility
+{
+    public class FlightService
+    {
+        public FlightService() { }
+
+        public void AddFlight(String companyName,
+                              String pointOfDeparture, DateTime departureTime, String departureGate,
+                              String pointOfArrival,   DateTime arrivalTime)
+        { 
+            
+        }
+          
+        public void DeleteFlight(Flight flight) { }
+        public void UpdateFlight(Flight flight) { }
+
+       
+    }
+}
