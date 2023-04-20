336c2d42b676704bcedf1deacfa5b1b80f63daa2
ayser <ayser@CENTAURI>
2023-04-20 21:43:33 +0300

    senkronizasyon

--- Solid Principles/LiskovSubstution/ICar.cs
+++ Solid Principles/LiskovSubstution/ICar.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LiskovSubstution
 {
-    public interface ICar
+    public interface ICar : IReservable
     {
 
     }
--- Solid Principles/LiskovSubstution/Reservation.cs
+++ Solid Principles/LiskovSubstution/Reservation.cs
@@ -32,5 +32,7 @@ namespace LiskovSubstution
                     throw new NotImplementedException();
             }
         }
+
+
     }
 }
