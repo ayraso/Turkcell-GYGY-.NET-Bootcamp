34f201348cc7d65a16887b2a1c79f071df82aabb
ayser <ayser@CENTAURI>
2023-04-20 22:32:58 +0300

    2d canvas creation

---
+++ Breakout Game/index.js
@@ -0,0 +1,9 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
+
